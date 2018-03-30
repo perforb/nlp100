c74ef68ec3fafb8f9f623f96563ddb91e33d8599
perforb <dev.perfumed.garden@gmail.com>
2018-03-30 11:06:34 +0900

    Added chapter1/02

---
+++ chapter1/02/zip.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def main():
+    letters1 = "パトカー"
+    letters2 = "タクシー"
+    print(''.join([a + b for a, b in zip(letters1, letters2)]))
+
+
+# def main():
+#     letters1 = "パトカー"
+#     letters2 = "タクシー"
+#     letters = ""
+#     for a, b in zip(letters1, letters2):
+#         letters += a + b
+#     print(letters)
+
+
+if __name__ == "__main__":
+    main()
