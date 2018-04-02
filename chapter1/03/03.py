07bc109ca69aeda99278fd6de0ad2ae73e3b86ae
perforb <dev.perfumed.garden@gmail.com>
2018-04-02 20:29:50 +0900

    Modified print format

--- chapter1/03/03.py
+++ chapter1/03/03.py
@@ -10,9 +10,10 @@ def main():
     after the heavy lectures involving quantum mechanics.
     '''
     sentence = re.sub('[,¥.]', '', sentence)
-    counter = [len(word) for word in sentence.split()]
-    print(counter)
+    pi = [len(word) for word in sentence.split()]
+    print('{0}.{1}'.format(pi[0], ''.join(str(number) for number in pi[1:])))
 
 
 if __name__ == "__main__":
     main()
+    # 3.14159265358979
