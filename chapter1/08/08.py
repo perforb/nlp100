1433a01f482767d5ff9b1b481d9a7d5bc16c852b
perforb <dev.perfumed.garden@gmail.com>
2018-04-08 19:45:55 +0900

    Refactored

--- chapter1/08/08.py
+++ chapter1/08/08.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import re
+
 MAGIC_NUMBER = 219
-BEGIN_CODE_POINT = ord('a')
-END_CODE_POINT = ord('z')
 
 
 def cipher(letters):
     '''
     Atbash - https://en.wikipedia.org/wiki/Atbash
     '''
-    encrypted_letters = []
-    for letter in letters:
-        code_point = ord(letter)
-        if code_point >= BEGIN_CODE_POINT and code_point <= END_CODE_POINT:
-            letter = chr(MAGIC_NUMBER - code_point)
-        encrypted_letters.append(letter)
-    return ''.join(encrypted_letters)
+    return re.sub(r'[a-z]',
+                  lambda m: chr(MAGIC_NUMBER - ord(m.group(0))),
+                  letters)
+
+
+# def cipher(letters):
+#     '''
+#     Atbash - https://en.wikipedia.org/wiki/Atbash
+#     '''
+#     begin_code_point = ord('a')
+#     end_code_point = ord('z')
+#     encrypted_letters = []
+#     for letter in letters:
+#         code_point = ord(letter)
+#         if code_point >= begin_code_point and code_point <= end_code_point:
+#             letter = chr(MAGIC_NUMBER - code_point)
+#         encrypted_letters.append(letter)
+#     return ''.join(encrypted_letters)
 
 
 def main():
