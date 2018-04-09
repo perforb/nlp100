a246278a9ed1d4db929f552409fbec595c3fae46
perforb <dev.perfumed.garden@gmail.com>
2018-04-09 21:55:06 +0900

    Added chapter2/10

---
+++ chapter2/10/10.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def number_of_lines(file_path):
+    with open(file_path, 'rt') as fh:
+        return sum(1 for i in fh)
+
+
+def main():
+    print(number_of_lines('../hightemp.txt'))
+
+
+if __name__ == "__main__":
+    main()
+    # 24
