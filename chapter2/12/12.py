6a290bb294f53454789c5b223e36c35dc948216f
perforb <dev.perfumed.garden@gmail.com>
2018-04-12 18:19:17 +0900

    Refacotored

--- chapter2/12/12.py
+++ chapter2/12/12.py
@@ -7,7 +7,7 @@ def readlines(file_path):
         return fh.readlines()
 
 
-def write_on(dest_path, contents):
+def write(dest_path, contents):
     with open(dest_path, mode='wt', encoding='utf-8') as fh:
         fh.write(contents)
 
@@ -16,9 +16,11 @@ def cut(lines, index=0):
     selected_columns = []
     for line in lines:
         if not line:
+            selected_columns.append('')
             continue
         columns = line.split()
         if len(columns) <= index:
+            selected_columns.append('')
             continue
         selected_columns.append(columns[index])
     return '\n'.join(selected_columns)
@@ -28,9 +30,11 @@ def main():
     lines = readlines('../hightemp.txt')
     col1 = cut(lines, 0)
     col2 = cut(lines, 1)
-    write_on('col1.txt', col1)
-    write_on('col2.txt', col2)
+    write('col1.txt', col1)
+    write('col2.txt', col2)
 
 
 if __name__ == "__main__":
     main()
+    # cat ../hightemp.txt | cut -f1
+    # cat ../hightemp.txt | cut -f2
