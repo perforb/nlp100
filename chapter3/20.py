c80da1124466c9ac34ebe970c420694b600fb496
perforb <dev.perfumed.garden@gmail.com>
2018-04-24 18:09:50 +0900

    Add chapter3/20

---
+++ chapter3/20.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+
+
+def read(file_path, title):
+    with open(file_path, encoding='utf-8') as fh:
+        for line in fh:
+            wiki = json.loads(line, encoding='utf-8')
+            if wiki['title'] == title:
+                return wiki
+
+    raise RuntimeError('Given title is not found. {0}'.format(title))
+
+
+def main():
+    wiki = read('./jawiki-country.json', 'イギリス')
+    print(wiki)
+
+
+if __name__ == "__main__":
+    main()
