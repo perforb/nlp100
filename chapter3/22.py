f019f0aacd06c262b4c08a1f0c5d42e927be15ac
perforb <dev.perfumed.garden@gmail.com>
2018-04-27 15:42:51 +0900

    refactor

--- chapter3/22.py
+++ chapter3/22.py
@@ -4,6 +4,8 @@
 import re
 import json
 
+CATEGORY_PATTERN = re.compile(r'^\[\[Category:(.*?)(\|.*)*\]\]$')
+
 
 def read(file_path, title):
     with open(file_path, encoding='utf-8') as fh:
@@ -15,15 +17,12 @@ def read(file_path, title):
     raise RuntimeError('specified title is not found. {0}'.format(title))
 
 
-def find_categories(file_path, title):
-    wiki = read('./jawiki-country.json', 'イギリス')
-    lines = wiki['text'].split('\n')
+def find_categories(lines):
     category_lines = [line for line in lines if 'Category' in line]
-    pattern = re.compile(r'^\[\[Category:(.*?)(\|.*)*\]\]$')
     categories = []
 
     for line in category_lines:
-        m = pattern.match(line)
+        m = CATEGORY_PATTERN.match(line)
         if m:
             categories.append(m.group(1))
 
@@ -31,7 +30,9 @@ def find_categories(file_path, title):
 
 
 def main():
-    categories = find_categories('./jawiki-country.json', 'イギリス')
+    wiki = read('./jawiki-country.json', 'イギリス')
+    lines = wiki['text'].split('\n')
+    categories = find_categories(lines)
     print('\n'.join(categories))
 
 
